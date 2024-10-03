e5bd8cd1be4062db1b65dabce241e4c81707f9e4
roy-sema <roy.hanley@semasoftware.com>
2024-10-03 17:34:27 +0100

    WIP 2

--- test.py
+++ test.py
@@ -4,3 +4,26 @@ from dataclasses import asdict
 
 from django.db.models import Case, IntegerField, When
 from rest_framework import serializers
+
+import logging
+import operator
+import os
+from functools import reduce
+
+from mvp.models import (
+    DataProviderConnection,
+    Organization,
+    Repository,
+    RepositoryCommit,
+    RepositoryCommitStatusChoices,
+)
+from django.conf import settings
+from django.db.models import Q
+from mvp.apis import GitHubApiConfig, AzureDevOpsApiConfig
+from mvp.integrations import (
+    get_git_provider_integration,
+    AzureDevOpsIntegration,
+    GitHubIntegration,
+)
+from mvp.tasks import DownloadRepositoriesTask
+from mvp.utils import run_command_subprocess
