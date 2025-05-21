f4fde1d2e548b22fe18423a7d37f6322c2f5bbe4
roy-sema <roy.hanley@semasoftware.com>
2025-05-21 10:35:23 +0100

    Amend example python 2

--- example_python_2.py
+++ example_python_2.py
@@ -1,106 +1,117 @@
-from datetime import date, datetime
+import base64
 
-from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import FormView
 
-from compass.codebasereports.widgets.sema_score_widget import SemaScoreWidget
-from compass.dashboard.views import DashboardView
-from mvp.models import AITypeChoices, Organization
-from mvp.services import (
-    AICompositionService,
-    ConnectedIntegrationsService,
-    ContextualizationService,
-    ContextualizationDayInterval,
-)
+from allauth.account import app_settings as account_settings
+from allauth.account.decorators import reauthentication_required
+from allauth.mfa import app_settings
+from allauth.mfa.adapter import get_adapter
+from allauth.mfa.internal.flows.add import redirect_if_add_not_allowed
+from allauth.mfa.models import Authenticator
+from allauth.mfa.totp.forms import ActivateTOTPForm, DeactivateTOTPForm
+from allauth.mfa.totp.internal import flows
+from allauth.mfa.utils import is_mfa_enabled
+from allauth.utils import get_form_class
 
 
-class AggregatedMessageService:
+@method_decorator(redirect_if_add_not_allowed, name="dispatch")
+@method_decorator(reauthentication_required, name="dispatch")
+class ActivateTOTPView(FormView):
+    form_class = ActivateTOTPForm
+    template_name = "mfa/totp/activate_form." + account_settings.TEMPLATE_EXTENSION
 
-    @classmethod
-    def get_for_day_interval(
-        cls, organization: Organization, day_interval: ContextualizationDayInterval,
-    ):
-        end = timezone.now()
-        start = end - timezone.timedelta(days=day_interval.value)
+    def dispatch(self, request, *args, **kwargs):
+        if is_mfa_enabled(request.user, [Authenticator.Type.TOTP]):
+            return HttpResponseRedirect(reverse("mfa_deactivate_totp"))
+        return super().dispatch(request, *args, **kwargs)
 
-        return {
-            "codebase_health": cls.get_codebase_health(
-                organization, start.date(), end.date()
-            ),
-            "percentage_ai": cls.get_percentage_ai(organization, start, end),
-            "initiatives": cls.get_initiatives(organization, day_interval),
-            "summary_insights": cls.get_summary_insights(
-                organization, start, end, day_interval
-            ),
-            "data_sets_used": cls.get_data_sets_used(organization),
-        }
-
-    @staticmethod
-    def get_codebase_health(organization: Organization, start: date, end: date):
-        score_widget = SemaScoreWidget(organization)
-        chart_score, _, _ = score_widget.get_charts(start, end)
-        return score_widget.get_score(chart_score)
-
-    @staticmethod
-    def get_percentage_ai(organization: Organization, start: datetime, end: datetime):
-        service = AICompositionService(organization)
-        cumulative_charts, daily_charts = service.get_charts(start, end)
-        ai_composition = service.get_composition(cumulative_charts)
-
-        return next(
-            (
-                item
-                for item in ai_composition
-                if item["label"] == AITypeChoices.OVERALL.label
-            ),
-            None,
+    def get_context_data(self, **kwargs):
+        ret = super().get_context_data(**kwargs)
+        adapter = get_adapter()
+        totp_url = adapter.build_totp_url(
+            self.request.user,
+            ret["form"].secret,
         )
-
-    @staticmethod
-    def get_initiatives(
-        organization: Organization, day_interval: ContextualizationDayInterval
-    ):
-        data, _ = ContextualizationService.load_output_data(
-            organization,
-            ContextualizationService.OUTPUT_FILENAME_ROADMAP,
-            day_interval=day_interval,
+        totp_svg = adapter.build_totp_svg(totp_url)
+        base64_data = base64.b64encode(totp_svg.encode("utf8")).decode("utf-8")
+        totp_data_uri = f"data:image/svg+xml;base64,{base64_data}"
+        ret.update(
+            {
+                "totp_svg": totp_svg,
+                "totp_svg_data_uri": totp_data_uri,
+                "totp_url": totp_url,
+            }
         )
-        return data.get("initiatives", [])
+        return ret
 
-    @staticmethod
-    def get_summary_insights(
-        organization: Organization,
-        start: datetime,
-        end: datetime,
-        day_interval: ContextualizationDayInterval
-    ):
-        # TODO: I'm not sure we're going to use as -
-        #  'We don't have Summary Insights talking about x% changes'.
-        #  If replaced by another pipeline remove this method.
+    def get_form_kwargs(self):
+        ret = super().get_form_kwargs()
+        ret["user"] = self.request.user
+        return ret
 
-        # TODO - super scrappy at the moment. If it is decided summary insights are to be
-        #  used in this way we should refactor DashboardView to be a service.
+    def get_form_class(self):
+        return get_form_class(app_settings.FORMS, "activate_totp", self.form_class)
 
-        # Doing this as DashboardView expects start
-        # and end dates to not be timezone aware.
-        start_date = start.replace(tzinfo=None)
-        end_date = end.replace(tzinfo=None)
+    def get_success_url(self):
+        if self.did_generate_recovery_codes:
+            return reverse("mfa_view_recovery_codes")
+        return reverse("mfa_index")
 
-        repositories = organization.repository_set.all()
-        count_data = DashboardView.get_count_data(organization, start_date, end_date)
-        data, _ = ContextualizationService.load_output_data(
-            organization,
-            ContextualizationService.OUTPUT_FILENAME_JUSTIFICATION,
-            day_interval=day_interval,
+    def form_valid(self, form):
+        totp_auth, rc_auth = flows.activate_totp(self.request, form)
+        self.did_generate_recovery_codes = bool(rc_auth)
+        return super().form_valid(form)
+
+
+activate_totp = ActivateTOTPView.as_view()
+
+
+@method_decorator(login_required, name="dispatch")
+class DeactivateTOTPView(FormView):
+    form_class = DeactivateTOTPForm
+    template_name = "mfa/totp/deactivate_form." + account_settings.TEMPLATE_EXTENSION
+    success_url = reverse_lazy("mfa_index")
+
+    def dispatch(self, request, *args, **kwargs):
+        self.authenticator = get_object_or_404(
+            Authenticator,
+            user=self.request.user,
+            type=Authenticator.Type.TOTP,
         )
-        if not data:
-            return None
+        if not is_mfa_enabled(request.user, [Authenticator.Type.TOTP]):
+            return HttpResponseRedirect(reverse("mfa_activate_totp"))
+        return self._dispatch(request, *args, **kwargs)
 
-        normalized_data = DashboardView.normalize_justification(data, count_data)
-        return DashboardView.enhance_justification(normalized_data, repositories)
+    @method_decorator(reauthentication_required)
+    def _dispatch(self, request, *args, **kwargs):
+        """There's no point to reauthenticate when MFA is not enabled, so the
+        `is_mfa_enabled` check needs to go first, which is why we cannot slap a
+        `reauthentication_required` decorator on the `dispatch` directly.
+        """
+        return super().dispatch(request, *args, **kwargs)
 
-    @staticmethod
-    def get_data_sets_used(organization: Organization):
-        return ConnectedIntegrationsService.get_connected_integrations_names(
-            organization, use_display_names=True
-        )
+    def get_form_kwargs(self):
+        ret = super().get_form_kwargs()
+        ret["authenticator"] = self.authenticator
+        # The deactivation form does not require input, yet, can generate
+        # validation errors in case deactivation is not allowed. We want to
+        # immediately present such errors even before the user actually posts
+        # the form, which is why we put an empty data payload in here.
+        ret.setdefault("data", {})
+        return ret
+
+    def get_form_class(self):
+        return get_form_class(app_settings.FORMS, "deactivate_totp", self.form_class)
+
+    def form_valid(self, form):
+        flows.deactivate_totp(self.request, self.authenticator)
+        return super().form_valid(form)
+
+
+deactivate_totp = DeactivateTOTPView.as_view()
 
