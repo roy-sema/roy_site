2c740b317d8b27c4e1db062cd47a8744b75b71a2
roy-sema <roy.hanley@semasoftware.com>
2025-09-02 16:01:28 +0100

    added example 9 + modified example 1

--- example_python_1.py
+++ example_python_1.py
@@ -1,173 +1,253 @@
-from typing import List, Union
-
-from django.urls import URLPattern, URLResolver, include, path
-
-from allauth.mfa import app_settings
-from allauth.mfa.webauthn import views
+# Django settings for example project.
+import os
+from pathlib import Path
 
 
-urlpatterns: List[Union[URLPattern, URLResolver]] = [
-    path("", views.list_webauthn, name="mfa_list_webauthn"),
-    path("add/", views.add_webauthn, name="mfa_add_webauthn"),
-    path(
-        "reauthenticate/",
-        views.reauthenticate_webauthn,
-        name="mfa_reauthenticate_webauthn",
-    ),
-    path(
-        "keys/<int:pk>/",
-        include(
-            [
-                path(
-                    "remove/",
-                    views.remove_webauthn,
-                    name="mfa_remove_webauthn",
-                ),
-                path(
-                    "edit/",
-                    views.edit_webauthn,
-                    name="mfa_edit_webauthn",
-                ),
-            ]
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DEBUG = True
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",  # Add 'postgresql_psycopg2', 'postgresql',
+        # 'mysql', 'sqlite3' or 'oracle'.
+        "NAME": os.environ.get(
+            "DATABASE_NAME", os.path.join(BASE_DIR / "db" / "example.db")
         ),
-    ),
+        "USER": "",  # Not used with sqlite3.
+        "PASSWORD": "",  # Not used with sqlite3.
+        "HOST": "",  # Set to empty string for localhost. Not used with sqlite3.
+        "PORT": "",  # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = "America/Chicago"
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = "en"
+
+LANGUAGES = [
+    ("ar", "Arabic"),
+    ("az", "Azerbaijani"),
+    ("bg", "Bulgarian"),
+    ("ca", "Catalan"),
+    ("cs", "Czech"),
+    ("da", "Danish"),
+    ("de", "German"),
+    ("el", "Greek"),
+    ("en", "English"),
+    ("es", "Spanish"),
+    ("et", "Estonian"),
+    ("eu", "Basque"),
+    ("fa", "Persian"),
+    ("fi", "Finnish"),
+    ("fr", "French"),
+    ("he", "Hebrew"),
+    ("hr", "Croatian"),
+    ("hu", "Hungarian"),
+    ("id", "Indonesian"),
+    ("it", "Italian"),
+    ("ja", "Japanese"),
+    ("ka", "Georgian"),
+    ("ko", "Korean"),
+    ("ky", "Kyrgyz"),
+    ("lt", "Lithuanian"),
+    ("lv", "Latvian"),
+    ("mn", "Mongolian"),
+    ("nb", "Norwegian Bokmål"),
+    ("nl", "Dutch"),
+    ("pl", "Polish"),
+    ("pt-BR", "Portuguese (Brazil)"),
+    ("pt-PT", "Portuguese (Portugal)"),
+    ("ro", "Romanian"),
+    ("ru", "Russian"),
+    ("sk", "Slovak"),
+    ("sl", "Slovenian"),
+    ("sr", "Serbian"),
+    ("sr-Latn", "Serbian (Latin)"),
+    ("sv", "Swedish"),
+    ("th", "Thai"),
+    ("tr", "Turkish"),
+    ("uk", "Ukrainian"),
+    ("zh-hans", "Chinese (Simplified)"),
+    ("zh-hant", "Chinese (Traditional)"),
 ]
 
-if app_settings.PASSKEY_LOGIN_ENABLED:
-    urlpatterns.append(path("login/", views.login_webauthn, name="mfa_login_webauthn"))
-if app_settings.PASSKEY_SIGNUP_ENABLED:
-    urlpatterns.append(
-        path("signup/", views.signup_webauthn, name="mfa_signup_webauthn")
-    )
+SITE_ID = 1
 
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
 
-import json
-from urllib.parse import parse_qs, quote, urlencode, urlparse, urlunparse
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
 
-from django import shortcuts
-from django.core.exceptions import ImproperlyConfigured
-from django.http import (
-    HttpRequest,
-    HttpResponseRedirect,
-    HttpResponseServerError,
-    QueryDict,
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ""
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ""
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = ""
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = "/static/"
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
-from django.urls import NoReverseMatch, reverse
 
-from allauth import app_settings as allauth_settings
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = "t8_)kj3v!au0!_i56#gre**mkg0&z1df%3bw(#5^#^5e_64!$_"
 
-def serialize_request(request):
-    return json.dumps(
-        {
-            "path": request.path,
-            "path_info": request.path_info,
-            "META": {k: v for k, v in request.META.items() if isinstance(v, str)},
-            "GET": request.GET.urlencode(),
-            "POST": request.POST.urlencode(),
-            "method": request.method,
-            "scheme": request.scheme,
-        }
-    )
+# List of callables that know how to import templates from various sources.
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "example" / "templates",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 
+MIDDLEWARE = (
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
+)
 
-def deserialize_request(s, request):
-    data = json.loads(s)
-    request.GET = QueryDict(data["GET"])
-    request.POST = QueryDict(data["POST"])
-    request.META = data["META"]
-    request.path = data["path"]
-    request.path_info = data["path_info"]
-    request.method = data["method"]
-    request._get_scheme = lambda: data["scheme"]
-    return request
+AUTHENTICATION_BACKENDS = ("allauth.account.auth_backends.AuthenticationBackend",)
 
+ROOT_URLCONF = "example.urls"
 
-def redirect(to):
-    try:
-        return shortcuts.redirect(to)
-    except NoReverseMatch:
-        return shortcuts.redirect(f"/{to}")
-
-
-def add_query_params(url: str, params: dict) -> str:
-    parsed_url = urlparse(url)
-    query_params = parse_qs(parsed_url.query)
-    query_params.update(params)
-    encoded_query = urlencode(query_params, doseq=True)
-    new_url = urlunparse(
-        (
-            parsed_url.scheme,
-            parsed_url.netloc,
-            parsed_url.path,
-            parsed_url.params,
-            encoded_query,
-            parsed_url.fragment,
-        )
-    )
-    return new_url
-
-
-def render_url(request, url_template, **kwargs):
-    url = url_template
-    for k, v in kwargs.items():
-        qi = url.find("?")
-        ki = url.find("{" + k + "}")
-        if ki < 0:
-            raise ImproperlyConfigured(url_template)
-        is_query_param = qi >= 0 and ki > qi
-        if is_query_param:
-            qv = urlencode({"k": v}).partition("k=")[2]
-        else:
-            qv = quote(v)
-        url = url.replace("{" + k + "}", qv)
-    p = urlparse(url)
-    if not p.netloc:
-        url = request.build_absolute_uri(url)
-    return url
-
-
-def default_get_frontend_url(request, urlname, **kwargs):
-    from allauth import app_settings as allauth_settings
-
-    if allauth_settings.HEADLESS_ENABLED:
-        from allauth.headless import app_settings as headless_settings
-
-        url = headless_settings.FRONTEND_URLS.get(urlname)
-        if allauth_settings.HEADLESS_ONLY and not url:
-            raise ImproperlyConfigured(f"settings.HEADLESS_FRONTEND_URLS['{urlname}']")
-        if url:
-            return render_url(request, url, **kwargs)
-    return None
-
-
-def get_frontend_url(request, urlname, **kwargs):
-    from allauth import app_settings as allauth_settings
-
-    if allauth_settings.HEADLESS_ENABLED:
-        from allauth.headless.adapter import get_adapter
-
-        return get_adapter().get_frontend_url(urlname, **kwargs)
-    return default_get_frontend_url(request, urlname, **kwargs)
+INSTALLED_APPS = (
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.humanize",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.admin",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.mfa",
+    "allauth.socialaccount.providers.dropbox",
+    "allauth.socialaccount.providers.dingtalk",
+    "allauth.socialaccount.providers.facebook",
+    "allauth.socialaccount.providers.edx",
+    "allauth.socialaccount.providers.evernote",
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.github",
+    "allauth.socialaccount.providers.linkedin_oauth2",
+    "allauth.socialaccount.providers.mediawiki",
+    "allauth.socialaccount.providers.openid",
+    "allauth.socialaccount.providers.openid_connect",
+    "allauth.socialaccount.providers.pinterest",
+    "allauth.socialaccount.providers.pocket",
+    "allauth.socialaccount.providers.reddit",
+    "allauth.socialaccount.providers.saml",
+    "allauth.socialaccount.providers.shopify",
+    "allauth.socialaccount.providers.slack",
+    "allauth.socialaccount.providers.snapchat",
+    "allauth.socialaccount.providers.soundcloud",
+    "allauth.socialaccount.providers.stackexchange",
+    "allauth.socialaccount.providers.telegram",
+    "allauth.socialaccount.providers.twitch",
+    "allauth.socialaccount.providers.twitter",
+    "allauth.socialaccount.providers.twitter_oauth2",
+    "allauth.socialaccount.providers.vimeo",
+    "allauth.socialaccount.providers.vimeo_oauth2",
+    "allauth.socialaccount.providers.weibo",
+    "allauth.socialaccount.providers.xing",
+    "allauth.usersessions",
+    "example.users",
+)
 
 
-def headed_redirect_response(viewname, query=None):
-    """
-    In some cases, we're redirecting to a non-headless view. In case of
-    headless-only mode, that view clearly does not exist.
-    """
-    try:
-        url = reverse(viewname)
-        if query:
-            url = add_query_params(url, query)
-        return HttpResponseRedirect(url)
-    except NoReverseMatch:
-        if allauth_settings.HEADLESS_ONLY:
-            # The response we would be rendering here is not actually used.
-            return HttpResponseServerError()
-        raise
+AUTH_USER_MODEL = "users.User"
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 9,
+        },
+    }
+]
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+ACCOUNT_LOGIN_BY_CODE_ENABLED = True
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION_BY_CODE_ENABLED = True
+ACCOUNT_LOGIN_METHODS = {
+    "email",
+}
+ACCOUNT_PASSWORD_RESET_BY_CODE_ENABLED = True
+ACCOUNT_SIGNUP_FIELDS = ["email*", "password1*", "password2*"]
+ACCOUNT_ADAPTER = "example.users.allauth.AccountAdapter"
 
 
-def is_headless_request(request: HttpRequest) -> bool:
-    return bool(getattr(getattr(request, "allauth", None), "headless", None))
+MFA_SUPPORTED_TYPES = [
+    "webauthn",
+    "totp",
+    "recovery_codes",
+]
+MFA_PASSKEY_LOGIN_ENABLED = True
+MFA_PASSKEY_SIGNUP_ENABLED = True
+
+try:
+    from .local_settings import *  # noqa
+except ImportError:
+    pass
 
